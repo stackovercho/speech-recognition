dee3cda557c1a8c10771e66de9488e20da7c2b22
Hyun <hyun.cho4@gmail.com>
2023-05-09 10:18:30 -0400

    connected textview, added SpeechRecognizer.

--- app/src/main/java/com/example/speechrecognition/MainActivity.kt
+++ app/src/main/java/com/example/speechrecognition/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.speechrecognition
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.SpeechRecognizer
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -14,10 +17,17 @@ const val MA = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var speechRecognizer: SpeechRecognizer
+    private lateinit var tv: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.tv)
+
+        // set up everything for speech recognition
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
 
         val voiceGranted: Int =
             ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
@@ -44,6 +54,44 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Log.w(MA, "permission not granted by user")
             }
+        }
+    }
+
+    inner class SpeechHandler : RecognitionListener {
+        override fun onReadyForSpeech(params: Bundle?) {
+            Log.d(MA, "inside onReadyForSpeech")
+        }
+
+        override fun onBeginningOfSpeech() {
+            Log.d(MA, "inside onBeginningOfSpeech")
+        }
+
+        override fun onRmsChanged(rmsdB: Float) {
+            Log.d(MA, "inside onRmsChanged")
+        }
+
+        override fun onBufferReceived(buffer: ByteArray?) {
+            Log.d(MA, "inside onBufferReceived")
+        }
+
+        override fun onEndOfSpeech() {
+            Log.d(MA, "inside onEndOfSpeech")
+        }
+
+        override fun onError(error: Int) {
+            Log.d(MA, "inside onError")
+        }
+
+        override fun onResults(results: Bundle?) {
+            Log.d(MA, "inside onResults")
+        }
+
+        override fun onPartialResults(partialResults: Bundle?) {
+            Log.d(MA, "inside onPartialResults")
+        }
+
+        override fun onEvent(eventType: Int, params: Bundle?) {
+            Log.d(MA, "inside onEvent")
         }
     }
 }
